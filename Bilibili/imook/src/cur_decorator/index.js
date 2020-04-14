7a7ed449409a26b0c99c5f4cbbc8130d03a7d615
zzh <zzhlovesly@outlook.com>
2020-04-14 23:43:01 +0800

    decorator functions

--- Bilibili/imook/src/cur_decorator/index.js
+++ Bilibili/imook/src/cur_decorator/index.js
@@ -25,11 +25,88 @@ class Decorator {
   }
 }
 
-function annotation(target) {
-  target.annotated = true;
-}
 
 let circle = new Circle('Yo');
 
 let deco = new Decorator(circle);
 deco.draw();
+
+function annotation(target) {
+  target.annotated = true;
+}
+
+// 带参数装饰器
+function testDec(isDec) {
+  return function(target) {
+    target.isDec = isDec;
+  }
+}
+
+@testDec
+class Demo2 {}
+
+// mixin
+function mixins(...list) {
+  return function(target) {
+    Object.assign(target.prototype, ...list);
+  }
+}
+
+const Foo = {
+  foo() {
+    alert('Foo');
+  }
+}
+
+@mixins(Foo)
+class Demo3 {}
+
+let obj3 = new Demo3();
+obj3.foo();
+
+
+// 装饰方法 1
+class Demo4 {
+  constructor() {
+    this.first = 'S';
+    this.last = 'L';
+  }
+  // 这为什么是一个属性啊，不应该是方法吗？
+  @readonly
+  name() {
+    return `${this.first} ${this.last}`;
+  }
+}
+
+function readonly(target, name, descriptor) {
+  descriptor.writable = false;
+  return descriptor;
+}
+
+const p = new Demo4();
+console.log(p.name());
+
+// 报错
+// p.name = 2;
+
+// 装饰方法 2
+class Demo5 {
+  @log
+  add(a, b) {
+    return a + b;
+  }
+}
+
+function log(target, name, descriptor) {
+  const oldVal = descriptor.value;
+  descriptor.value = function() {
+    // ? arguments log 不出来么？
+    console.log(arguments);
+    console.log(`calling [${name}] with ${arguments}`);
+    return oldVal.apply(this, arguments);
+  };
+  return descriptor;
+}
+
+const obj5 = new Demo5();
+obj5.add(3, 4);
