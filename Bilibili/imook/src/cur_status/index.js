e52b3c957e83f1d1554fe419daf095c3187b2dbf
Zihan Zhang <zzhlovesly@outlook.com>
2020-05-03 23:36:19 +0800

    state pattern

---
+++ Bilibili/imook/src/cur_status/index.js
@@ -0,0 +1,31 @@
+class State {
+    constructor(color) {
+        this.color = color;
+    }
+
+    handle(ctx) {
+        console.log(`turn to ${this.color} light`);
+        ctx.setState(this);
+    }
+}
+
+class Context {
+    constructor() {
+        this.state = null;        
+    }
+    getState() {
+        return this.state;
+    }
+    setState(state) {
+        this.state = state;
+    }
+}
+
+let context = new Context();
+
+let green = new State('green');
+let yellow = new State('yellow');
+let red = new State('red');
+
+//  这个调用方式真是奇葩。。。
+green.handle(context);
