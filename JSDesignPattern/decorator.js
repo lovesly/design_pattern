dff1b43c8f04e5d875e05ad0362cb640d0c0cf02
Zihan Zhang <zzhlovesly@outlook.com>
2020-06-06 23:57:44 +0800

    example2 of decorator

--- JSDesignPattern/decorator.js
+++ JSDesignPattern/decorator.js
@@ -22,3 +22,41 @@ truck.setColor('blue');
 // 装饰的是实例，不会影响 prototype
 const secondInst = new Vehicle();
 console.log(secondInst);
+
+// ============ v2 ============== /
+function Macbook() {
+    this.cost = function() {
+        return 997;
+    };
+    this.screenSize = function() {
+        return 11.6;
+    }
+}
+
+function Memory(macbook) {
+    const v = macbook.cost();
+    macbook.cost = function() {
+        return v + 75;
+    };
+}
+
+function Engraving(macbook) {
+    const v = macbook.cost();
+    macbook.cost = function() {
+        return v + 200;
+    };
+}
+
+function Insurance(macbook) {
+    const v = macbook.cost();
+    macbook.cost = function() {
+        return v + 250;
+    };
+}
+
+const mb = new Macbook();
+Memory(mb);
+Engraving(mb);
+Insurance(mb);
+
+console.log(mb.cost());
